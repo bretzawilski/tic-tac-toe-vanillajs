0a835ba0cef0fb4613d84976c0bc2a6d9dbe33ca
Bret Zawilski <bret.zawilski@gmail.com>
2021-05-07 14:12:33 +0100

    Updated script. Game board is responsive to clicks.

--- script.js
+++ script.js
@@ -1,28 +1,73 @@
+// Functions controlling the state of the gameBoard
 const gameBoard = (() => {
+  
   const board = [
-    'X', 'O', 'X',
-    'X', 'O', 'O',
-    'O', 'X', 'O',
+    '', '', '',
+    '', '', '',
+    '', '', '',
   ];
-    return { board };
+    
+  const resetGame = (() => {
+    
+    for (i = 0; i < play.board.length ; i++) {
+      play.board[i] = '';
+    }
+
+    start.render();
+
+    // board = [
+    //   '', '', '',
+    //   '', '', '',
+    //   '', '', '',
+    // ];
+  });
+
+  return { 
+    board,
+    resetGame,
+  };
 });
 
+// Functions concerning the operation of the game itself (the engine?)
 const game = (() => {
+
+  let turn = true;
+
   const render = (() => {
-    for (i = 0; i < 9; i++) {
+    for (i = 0; i < play.board.length; i++) {
       let square = document.getElementById('b-' + i);
       let marker = play.board[i];
       square.innerText = marker;
     }
   });
-  return { render };
+
+  const placeMarker = ((event) => {
+    const pos = event.target.id.split('-')[1];
+        
+    play.board[pos] = turn ? 'X' : 'O';
+    turn = !turn;
+    
+    render();
+  })
+
+  return { 
+    render,
+    placeMarker,
+  };
+});
+
+// Functions relating to individual Players
+const Player = (() => {
+
 });
 
 
-const Player = (name, piece) => {
-  const getPiece = () => piece;
-  const getName = () => name;
-}
-
+// Global constants and event listeners. How do I move these out of global?
 const start = game();
 const play = gameBoard();
+
+const boardSquare = document.getElementById('game');
+boardSquare.addEventListener('click', start.placeMarker);
+
+const resetButton = document.getElementById('reset-button');
+resetButton.addEventListener('click', play.resetGame);
